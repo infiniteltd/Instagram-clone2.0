1e6eae796f830a0058364c21eeb515320d60602b
infiniteltd <chrissamps24@gmail.com>
2024-04-01 12:07:02 +0100

    added new functions to modal component

--- components/Modal.js
+++ components/Modal.js
@@ -43,6 +43,7 @@ function Modal() {
             setOpen(false);
             setLoading(false);
             setSelectedFile(null);
+
         } catch (error) {
             console.error('Error uploading post:', error.message);
         }
@@ -89,12 +90,12 @@ function Modal() {
                         as={Fragment}
                         enter='ease-out duration-300'
                         enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-                        enterTo='opacity-100 translate-y-0 sm:scale-95'
+                        enterTo='opacity-100 translate-y-0 sm:scale-100'
                         leave='ease-in duration-200'
                         leaveFrom='opacity-100 translate-y-0 sm:scale-100'
                         leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                     >
-                        <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
+                        <div className='inline-block  align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
                             <div>
                                 {selectedFile ? (
                                     <img src={selectedFile}
